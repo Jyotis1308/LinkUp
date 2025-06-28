17d44042dcd9775226bde530b8f50a44b6faa709
unknown <jyoti@gmail.com>
2025-06-28 12:05:21 +0530

    initial commit

---
+++ frontend/vite-project/src/hooks/useSignUp.js
@@ -0,0 +1,21 @@
+
+import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { signup } from "../lib/api";
+
+const useSignUp = () => {
+ const queryClient = useQueryClient();
+  const navigate=useNavigate();
+
+ const {mutate,isPending,error}=useMutation({
+  mutationFn:signup,
+  onSuccess: () => {
+    queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    navigate("/"); 
+  },
+  })
+return {isPending, error, signupMutation:mutate};
+}
+
+export default useSignUp;
